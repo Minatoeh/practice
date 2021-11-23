ecb26289e6a87196daa343c57c09448768d5404c
Minatoeh <minatoeheh@gmai.com>
2021-11-23 16:11:54 +0200

    master

--- task2/1.4.rb
+++ task2/1.4.rb
@@ -1,1 +1,14 @@
-1.4.rb
+a = 'Earth'
+b = 'Russia'
+c = 'Moscow'
+new_list = [a, b, c ]
+print new_list
+
+
+# new_list = ['Earth', 'Russia', 'Moscow']
+# a = new_list[0]
+# b = new_list[1]
+# c = new_list[2]
+# puts  [a, b, c ]
+
+# incorrect way to solution
