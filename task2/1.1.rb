558debea09261698e8a6f1984b53a9da9cf3ebad
Minatoeh <minatoeheh@gmai.com>
2021-11-23 15:49:39 +0200

    master

---
+++ task2/1.1.rb
@@ -0,0 +1,6 @@
+Class Array
+
+puts "Lets make some random numbers"
+print Array.new(6) { rand(1..500)  }
+Array.new = Array.sort
+print Array.sort
---
+++ task2/1.2.rb
@@ -0,0 +1,3 @@
+puts " Let's make 'strange vocabulary' : "
+a = gets.to_i
+puts a  ' a '
