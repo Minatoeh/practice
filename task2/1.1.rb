39ec55ac18f577e695e595a7a3b1f34ed3c13d3c
Minatoeh <minatoeheh@gmai.com>
2021-11-23 17:54:58 +0200

    master

--- task2/1.1.rb
+++ task2/1.1.rb
@@ -1,5 +1,3 @@
-Class Array
-
 puts "Lets make some random numbers"
 print Array.new(6) { rand(1..500)  }
 Array.new = Array.sort
--- task2/1.6.rb
+++ task2/1.6.rb
@@ -1,1 +1,7 @@
-1.6.rb
+x = (1..100).select { |n| n % 7 == 0 } 
+print x
+
+
+
+y = (1..100).select { |n| n % 7 != 0 }
+print y
