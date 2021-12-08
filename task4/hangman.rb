d66e9869c33db35490841e08b3d6fa7235dd2b54
Minatoeh <minatoeheh@gmai.com>
2021-12-08 10:14:20 +0200

    master

---
+++ task4/hangman.rb
@@ -0,0 +1,15 @@
+class Hangman
+	def initialize
+		puts "Давайте сыграем с Вами в 'Виселицу' !"
+		sleep 2
+		puts "Выбирайте категорию, из которой хотите получить вопрос. "
+		vocabulary
+		
+	end
+
+
+
+end
+
+
+Hangman.new
