faf37ad3e86d782728c04c7fd744244aca4ec408
Minatoeh <minatoeheh@gmai.com>
2021-12-20 19:32:55 +0200

    master

--- task4/hangman.rb
+++ task4/hangman.rb
@@ -1,4 +1,4 @@
-reqiure "./methods_for_hangman.rb"
+require "./methods_for_hangman.rb"
 puts "Давайте поиграем с Вами в Виселицу !"
 sleep 2
 puts "Игрок №1, загадывайте слово: "
@@ -13,13 +13,16 @@ good = user_input.split(//)
 bad =[]
 mistake = 0 
 
-while mistake >7
+while mistake < 7 do
 	puts "Сделайте Ваш выбор"
 	input = gets.chomp
-	if input.include?(good)
+	if good.include?(input)
 		good.insert(input)
+	elsif good == good
+		puts "Поздравляем, Вы выиграли ! "
 	else
 		mistake += 1
+	break
 	end
 	
 end
