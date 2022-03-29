0c11c71309b376dc512cb63e38138a91f1cd2789
Minatoeh <minatoeheh@gmai.com>
2022-03-29 16:46:37 +0300

    master

---
+++ task5/person.rb
@@ -0,0 +1,30 @@
+class Person
+	def initialize(age, name)
+		@age = age
+		@name = name
+	end
+
+	def know(person)
+
+	end
+
+	def is_known(person)
+
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# *ЗАДАЧА 1:
+# Реализовать класс Person, у которого должно быть два публичных поля: age и name. 
+# Также у него должен быть следующий набор методов: know(person), который позволяет 
+# добавить другого человека в список знакомых. И метод is_known(person), который возвращает знакомы ли два человека
